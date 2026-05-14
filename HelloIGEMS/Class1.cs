6e199f4795d1e86179d92984e8a2f2aae59d927c
Your Name <your_email@example.com>
2026-05-14 13:25:37 -0400

    Working BoundaryScan version before freeze fixes

---
+++ HelloIGEMS/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace HelloIGEMS
+{
+    public class Hello
+    {
+        public static void DLLMain()
+        {
+            string name = Interaction.InputBox(
+                "What is your name?",
+                "Name Input",
+                "Ryan"
+            );
+
+            MessageBox.Show("Hello " + name + "!");
+        }
+    }
+}
